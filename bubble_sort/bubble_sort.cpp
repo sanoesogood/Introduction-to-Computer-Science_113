dfa0b7dc43eb99ed91576f3b2636b83dbb970e3d
sanoe <135816919+sanoesogood@users.noreply.github.com>
2025-03-27 20:50:41 +0800

    modify typesetting

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -15,7 +15,7 @@ int least_compare = 0;    // 陣列排序完當下的比較次數
 int exchange = 0;
 
 int main(){
-	int *data = nullptr;
+    int *data = nullptr;
     int n;
 
     /* set random seed */
@@ -37,12 +37,12 @@ int main(){
     display(data, n);
     printf("\n");
 
-	/* sort */
-	bubblesort(data, n);
+    /* sort */
+    bubblesort(data, n);
 
-	/* output */
+    /* output */
     printf("sorted array by bubble sort:\n");
-	display(data, n);
+    display(data, n);
     printf("\n");
 
     printf("compare times: %d\n", compare);
@@ -52,40 +52,40 @@ int main(){
 
     delete [] data;
 
-	return 0;
+    return 0;
 }
 
 
 void bubblesort(int *data, int n){
-	int i, j, temp;
+    int i, j, temp;
     bool is_exchange = false;
 
-	for(j=n-2; j>=0; j--){
+    for(j=n-2; j>=0; j--){
         is_exchange = 0;
 
-		/* scan */
-		for(i=0; i<=j; i++){
-			if(data[i] > data[i+1]){
-				/* exchange */
-				temp = data[i];
-				data[i] = data[i+1];
-				data[i+1] = temp;
+	/* scan */
+	for(i=0; i<=j; i++){
+	    if(data[i] > data[i+1]){
+	        /* exchange */
+		    temp = data[i];
+		    data[i] = data[i+1];
+		    data[i+1] = temp;
 
-                is_exchange = true;
-                exchange++;
-			}
+                    is_exchange = true;
+                    exchange++;
+	    }
             compare++;
-		}
+	}
 
         if(!is_exchange){
             least_compare = compare - (j + 1);    // 因為 for 迴圈中, 從 i=0 到 i<=j, 共執行 j+1 次
             break;
         }
-	}
+    }
 }
 
 void display(int *data, int n){
-	for(int i=0; i<n; i++)
-		printf("%d ", data[i]);
+    for(int i=0; i<n; i++)
+        printf("%d ", data[i]);
     printf("\n");
 }
