bc6e29d81711f8f7d27826b2d39c869c7bcda46b
sanoe <sanoesogood@gmail.com>
2025-05-30 13:12:08 +0800

    change indent to 4 space & final edition

--- 1132_final_exam/problem_2/cdllist.cpp
+++ 1132_final_exam/problem_2/cdllist.cpp
@@ -36,12 +36,12 @@
 #include <stdlib.h>
 
 struct student{
-	char name[10];
-	int no;
-	int mid;
-	int final;
-	struct student *next;
-	struct student *prev;
+    char name[10];
+    int no;
+    int mid;
+    int final;
+    struct student *next;
+    struct student *prev;
 };
 struct student *top = NULL;
 
@@ -57,249 +57,249 @@ void show_memory();
 #endif
 
 int main(){
-  	int choice = 0;
+      int choice = 0;
 
-  	while(1){
-		printf("1: create list\n");
-		printf("2: query\n");
-		printf("3: insert\n");
-		printf("4: delete\n");
-		printf("5: show list by ascending order\n");
-		printf("6: show list by descending order\n");
-		printf("7: quit\n");
-		#if DEBUG
-		printf("#debug\n");
-		printf("8: show memory\n\n");
-		#endif
-		printf("\nchoice: ");
+      while(1){
+        printf("1: create list\n");
+        printf("2: query\n");
+        printf("3: insert\n");
+        printf("4: delete\n");
+        printf("5: show list by ascending order\n");
+        printf("6: show list by descending order\n");
+        printf("7: quit\n");
+        #if DEBUG
+        printf("#debug\n");
+        printf("8: show memory\n\n");
+        #endif
+        printf("\nchoice: ");
 
-		scanf("%d", &choice);
+        scanf("%d", &choice);
 
-		switch(choice){
-		case 1:	
-			printf("=======1: create\n");
-			create();
-			break;
-		case 2:	
-			printf("========2: query\n");
-			query();
-			break;
-		case 3:	
-			printf("========3: insert\n");
-			insert();
-			break;
-		case 4:	
-			printf("========4: delete\n");
-			delete_data();
-			break;
-		case 5:	
-			printf("========5: show by ascending order\n");
-			show();
-			break;
-		case 6:
-			printf("========6: show by descending order\n");
-			show_inverse();
-			break;
-		case 7: 
-			printf("========7: quit\n");
-			return 0;
-		#if DEBUG
-		case 8:
-			printf("========8: show memeory\n");
-			show_memory();
-			break;
-		#endif
-		default:
-			printf("========-1: choice not found.\n");
-			return 0;				
-		}
-		printf("========\n");
+        switch(choice){
+        case 1:	
+            printf("=======1: create\n");
+            create();
+            break;
+        case 2:	
+            printf("========2: query\n");
+            query();
+            break;
+        case 3:	
+            printf("========3: insert\n");
+            insert();
+            break;
+        case 4:	
+            printf("========4: delete\n");
+            delete_data();
+            break;
+        case 5:	
+            printf("========5: show by ascending order\n");
+            show();
+            break;
+        case 6:
+            printf("========6: show by descending order\n");
+            show_inverse();
+            break;
+        case 7: 
+            printf("========7: quit\n");
+            return 0;
+        #if DEBUG
+        case 8:
+            printf("========8: show memeory\n");
+            show_memory();
+            break;
+        #endif
+        default:
+            printf("========-1: choice not found.\n");
+            return 0;				
+        }
+        printf("========\n");
     }
-	return 0;	
+    return 0;	
 }
 
 void create(){ 	
-	struct student *x = NULL;
-	FILE *fp = NULL;
+    struct student *x = NULL;
+    FILE *fp = NULL;
 
-	fp = fopen("student.txt", "r");
-	if(fp == NULL){
-		printf("========-1: error opening file.");
-		return;
-	}
+    fp = fopen("student.txt", "r");
+    if(fp == NULL){
+        printf("========-1: error opening file.");
+        return;
+    }
 
-	while(1){
-		x = new struct student[1];
+    while(1){
+        x = new struct student[1];
 
-		if(fscanf(fp, "%s %d %d %d", x->name, &x->no, &x->mid, &x->final) != 4){
-			break;
-		}
+        if(fscanf(fp, "%s %d %d %d", x->name, &x->no, &x->mid, &x->final) != 4){
+            break;
+        }
 
-		#if DEBUG
-		printf("read: %s %d %d %d\n", x->name, x->no, x->mid, x->final);
-		#endif
+        #if DEBUG
+        printf("read: %s %d %d %d\n", x->name, x->no, x->mid, x->final);
+        #endif
 
-		if(top == NULL){
-			x->next = x;
-			x->prev = x;
-			top = x;
-		}
-		else{
-			x->next = top;
-			x->prev = top->prev;
-			top->prev->next = x;
-			top->prev = x;
-			top = x;
-		}
-	}
+        if(top == NULL){
+            x->next = x;
+            x->prev = x;
+            top = x;
+        }
+        else{
+            x->next = top;
+            x->prev = top->prev;
+            top->prev->next = x;
+            top->prev = x;
+            top = x;
+        }
+    }
 
-	printf("created successfully.\n");
-	fclose(fp);
+    printf("created successfully.\n");
+    fclose(fp);
 
-	return;
+    return;
 }
 
 void query(){
-	int key;
-	struct student *temp = top;
+    int key;
+    struct student *temp = top;
 
-	if(top == NULL){
-		printf("no data in the list.\n");
-		return;
-	}
+    if(top == NULL){
+        printf("no data in the list.\n");
+        return;
+    }
 
-	scanf("%d", &key);
+    scanf("%d", &key);
 
-	do{
-		if(temp->no == key){
-			printf("%s %d %d %d\n", temp->name, temp->no, temp->mid, temp->final);
-			return;	
-		}
-		temp = temp->next;
-	}while(temp != top);
+    do{
+        if(temp->no == key){
+            printf("%s %d %d %d\n", temp->name, temp->no, temp->mid, temp->final);
+            return;	
+        }
+        temp = temp->next;
+    }while(temp != top);
 
-	printf("key = %d not found.\n", key);
+    printf("key = %d not found.\n", key);
 
-	return;
+    return;
 }
 
 void insert(){
-	struct student *x = NULL;
+    struct student *x = NULL;
 
-	x = new struct student[1];
+    x = new struct student[1];
 
-	printf("format: name no mid final\n");
-	scanf("%s %d %d %d", x->name, &x->no, &x->mid, &x->final);
-	if(top == NULL){
-		x->next = x;
-		x->prev = x;
-		top = x;
-	}
-	else{
-		x->next = top;
-		x->prev = top->prev;
-		top->prev->next = x;
-		top->prev = x;
-		top = x;
-	}
+    printf("format: name no mid final\n");
+    scanf("%s %d %d %d", x->name, &x->no, &x->mid, &x->final);
+    if(top == NULL){
+        x->next = x;
+        x->prev = x;
+        top = x;
+    }
+    else{
+        x->next = top;
+        x->prev = top->prev;
+        top->prev->next = x;
+        top->prev = x;
+        top = x;
+    }
 
-	return;
+    return;
 }
 
 void delete_data(){
-	if(top == NULL){
-		printf("no data in the list.\n");
-		return;
-	}
+    if(top == NULL){
+        printf("no data in the list.\n");
+        return;
+    }
 
-	int key;
-	struct student *temp = top, *prev = top;
+    int key;
+    struct student *temp = top, *prev = top;
 
-	scanf("%d", &key);
-	
-	do{
-		if(temp->no == key){
-			if(temp->next == temp){
-				delete[] temp;
-				top = NULL;
-			}
-			// // 後來發現不需要特別為刪除開頭寫一個方法
-			// else if(temp == top){
-			// 	top->prev->next = top->next;
-			// 	top->next->prev = top->prev;
-			// 	top = top->next;
-			// 	delete[] temp;
-			// }
-			else{
-				prev = temp->prev;
-				prev->next = temp->next;
-				temp->next->prev = prev;
-				if(temp == top){
-					top = temp->next;
-				}
-				delete[] temp;
-			}
+    scanf("%d", &key);
+    
+    do{
+        if(temp->no == key){
+            if(temp->next == temp){
+                delete[] temp;
+                top = NULL;
+            }
+            // // 後來發現不需要特別為刪除開頭寫一個方法
+            // else if(temp == top){
+            // 	top->prev->next = top->next;
+            // 	top->next->prev = top->prev;
+            // 	top = top->next;
+            // 	delete[] temp;
+            // }
+            else{
+                prev = temp->prev;
+                prev->next = temp->next;
+                temp->next->prev = prev;
+                if(temp == top){
+                    top = temp->next;
+                }
+                delete[] temp;
+            }
 
-			printf("delete key = %d\n", key);
-			return;	
-		}
-		temp=temp->next;
-	}while(temp != top);
+            printf("delete key = %d\n", key);
+            return;	
+        }
+        temp=temp->next;
+    }while(temp != top);
 
-	printf("key = %d not found.\n", key);
-	
-	return;
+    printf("key = %d not found.\n", key);
+    
+    return;
 }
 
 // 按插入時間顯示 (最新插入的在最前面)
 void show(){
-	if(top == NULL){
-		printf("no data in the list.\n");
-		return;
-	}
+    if(top == NULL){
+        printf("no data in the list.\n");
+        return;
+    }
 
-	struct student *temp = top;
+    struct student *temp = top;
 
-	do{
-		printf("%s %d %d %d\n", temp->name, temp->no, temp->mid, temp->final);
-		temp = temp->next;
-	}while(temp != top);
+    do{
+        printf("%s %d %d %d\n", temp->name, temp->no, temp->mid, temp->final);
+        temp = temp->next;
+    }while(temp != top);
 
-	return;
+    return;
 }
 
 // 按插入時間顯示 (最早插入的在最前面)
 void show_inverse(){
-	if(top == NULL){
-		printf("no data in the list.\n");
-		return;
-	}
+    if(top == NULL){
+        printf("no data in the list.\n");
+        return;
+    }
 
-	struct student *temp = top->prev;
+    struct student *temp = top->prev;
 
     do{
         printf("%s %d %d %d\n", temp->name, temp->no, temp->mid, temp->final);
         temp = temp->prev;
     }while(temp != top->prev);
 
-	return;
+    return;
 }
 
 #if DEBUG
 void show_memory(){
-	struct student *temp = top;
+    struct student *temp = top;
 
-	if(temp == NULL){
-		printf("no data in the list.\n");
-		return;
-	}
+    if(temp == NULL){
+        printf("no data in the list.\n");
+        return;
+    }
 
-	do{
-		printf("x_name: %s, x_no: %d, x_mem: %p, p: %p, n: %p\n", \
-			    temp->name, temp->no, temp, temp->prev, temp->next);
-		temp = temp->next;
-	}while(temp != top);
+    do{
+        printf("x_name: %s, x_no: %d, x_mem: %p, p: %p, n: %p\n", \
+                temp->name, temp->no, temp, temp->prev, temp->next);
+        temp = temp->next;
+    }while(temp != top);
 
-	return;
+    return;
 }
 #endif
